b76eacb3a3a4bdbf4f5e8bb0cb262c088275a621
Jhonatan <Jhonatan@Jhonatan-PC>
2017-08-18 06:12:02 -0300

    UHUL ASSIM E MAIS FACIL DA UM BIZU AI

--- listaOne/Ex07.py
+++ listaOne/Ex07.py
@@ -3,25 +3,22 @@ Created on 18 de ago de 2017
 
 @author: Jhonatan
 '''
-resp= []
-print("pressione enter 2X para NÃO (s) para SIM")
-
+resp = 0
+print("responda as questoes a baixo com 0,1,2,3,4 e 5 lembre-se so coloque as que forem verdadeiras pois isso é um julgamento\n")
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+print("Telefonou para a vítima?\n")
+print("Esteve no local do crime?\n")
+print("Mora perto da vítima?\n")
+print("Devia para a vítima?\n")
+print("Já trabalhou com a vítima?\n")
 for i in range(1):
-      resp = input("Telefonou para a vítima?")
-      resp = input("Esteve no local do crime?")
-      resp = input("Mora perto da vítima?")
-      resp = input("Devia para a vítima?")
-      resp = input("Já trabalhou com a vítima?")
-for i in range(1): 
-  cont = 0 
-  for i in range(1): 
-    if (resp=='s' or resp=='S'): 
-      cont = cont + 1 
-      if(cont==2): 
-          print("A pessoa" , i + 1 , "é suspeita")
-      if(cont == 5): 
-          print("A pessoa",i + 1,"é a assassina") 
-      if(cont == 3 or cont ==4): 
-          print("A pessoa",i + 1,"é cumplice") 
-      if(cont <= 1) : 
-          print("A pessoa",i + 1,"é inocente") 
+  resp = int(input()) 
+  if(resp <= 1): 
+    print("A pessoa é inocente") 
+  if(resp==2): 
+    print("A pessoa é suspeita")
+  if(resp == 3 or resp == 4): 
+    print("A pessoa é cumplice") 
+  if(resp == 5): 
+    print("A pessoa é a assassina MEIRINHO PRENDA ELA") 
+      
